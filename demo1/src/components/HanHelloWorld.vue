306c13e12f45b11a79f69e6d69dc6bbcb10b8786
TheLastMorty <1483625333@qq.com>
2023-05-12 16:41:06 +0800

    new file:   demo1/.gitignore 	new file:   demo1/README.md 	new file:   demo1/babel.config.js 	new file:   demo1/jsconfig.json 	new file:   demo1/package-lock.json 	new file:   demo1/package.json 	new file:   demo1/public/favicon.ico 	new file:   demo1/public/index.html 	new file:   demo1/src/App.vue 	new file:   demo1/src/assets/logo.png 	new file:   demo1/src/components/HanHelloWorld.vue 	new file:   demo1/src/components/HelloWorld.vue 	new file:   demo1/src/main.js 	new file:   demo1/vue.config.js HelloWorld

---
+++ demo1/src/components/HanHelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>hello world</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VueDemoHanHelloWorld',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

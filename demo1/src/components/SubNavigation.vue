5eff64db70644aeaad8a0f8de2a7e7030f332c31
TheLastMorty <1483625333@qq.com>
2023-05-20 15:06:14 +0800

    导航栏下拉选项

--- demo1/src/components/SubNavigation.vue
+++ demo1/src/components/SubNavigation.vue
@@ -1,30 +1,37 @@
 <template>
-    <el-submenu index=100>
+    <el-submenu :index=index>
       <template slot="title">
         <i class="el-icon-location"></i>
-        <span>导航一</span>
+        <span>车间选择</span>
       </template>
       <el-menu-item-group>
-        <el-menu-item v-for="(name,index) in names"></el-menu-item>
-
+        <NavigationItem v-for="(item,sub_index) in items" 
+          :index=sub_index :NavigationName=item.name :to=item.to :key=sub_index
+        >
+        </NavigationItem>
 
       </el-menu-item-group>
     </el-submenu>
 </template>
 <script>
+import NavigationItem from './NavigationItem.vue';
+
 export default {
-   data() {
-      return {
-      }
-   },
-   created(){
-   },
-   computed:{
-   },
-   methods:{
-   },
-   props:["names","index"]
-   
+    data() {
+        return {
+          items:[]
+        };
+    },
+    created() {
+    },
+    computed: {},
+    methods: {},
+    props: ["index"],
+    components: { NavigationItem },
+    beforeMount(){
+      // 发送ajax请求获取items
+      this.items=[{name:"Han",to:"Han"},{name:"Org",to:"Org"}]
+    }
 }
 </script>
 <style lang="scss" scoped>
